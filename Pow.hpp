e9035292969e9af1dd2f9f499e3f7422cbb771d9
RDanover <danover.rachel@gmail.com>
2021-11-02 11:45:55 -0700

    Updated Pow.hpp

--- Pow.hpp
+++ Pow.hpp
@@ -2,12 +2,12 @@
 #define __POW_HPP__
 
 #include "base.hpp"
-
+#include "math.h"
 class Pow : public Base {
     public:
         Pow();
-        virtual double evaluate(){0.0;} //placeholder
-        virtual std::string stringify() { "**"; }//placeholder
+        virtual double evaluate(){pow("leftOperand".evaluate(),"rightOPerand".evaluate());} //placeholder
+        virtual std::string stringify() {"( " << "leftoperand".stringify() << "**" << "rightoperand".stringify() << " )" ; }//placeholder
 };
 
 #endif 
