841f37ccf68d669b55a17f08c95bbf02b551a7d6
RDanover <danover.rachel@gmail.com>
2021-11-02 10:17:54 -0700

    created a mock

---
+++ SevenOpMock.hpp
@@ -0,0 +1,13 @@
+#ifndef __SEVENOPMOCK_HPP__
+#define __SEVENOPMOCK_HPP__
+
+#include "base.hpp"
+
+class SevenOPMock : public Base {
+    public:
+        SevenOpMock() { }
+        virtual double evaluate() { return 7.5; }
+        virtual std::string stringify() { return "7.5"; }
+};
+
+#endif _
