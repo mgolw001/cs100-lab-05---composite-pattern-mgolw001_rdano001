a59acac8b9c79d6fb0d635c0c655fd79ed465656
RDanover <danover.rachel@gmail.com>
2021-11-02 10:24:01 -0700

    updated SevenOpMock

--- SevenOpMock.hpp
+++ SevenOpMock.hpp
@@ -5,7 +5,7 @@
 
 class SevenOPMock : public Base {
     public:
-        SevenOpMock() { }
+        SevenOPMock() { }
         virtual double evaluate() { return 7.5; }
         virtual std::string stringify() { return "7.5"; }
 };
