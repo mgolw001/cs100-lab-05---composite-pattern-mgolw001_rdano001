9f7bc9c99a62d8f1bf8dff621f8a7a218d3dc2b1
RDanover <danover.rachel@gmail.com>
2021-11-02 10:37:12 -0700

    created mult.hpp

---
+++ Mult.hpp
@@ -0,0 +1,13 @@
+#ifndef __MULT_HPP__
+#define __MULT_HPP__
+
+#include "base.hpp"
+
+class Mult : public Base {
+    public:
+        Mult();
+        virtual double evaluate(){0.0;} //placeholder
+        virtual std::string stringify() { "*"; }//placeholder
+};
+
+#endif _
