0dd49151490d27eaa34406a8efa564e24462c44f
RDanover <danover.rachel@gmail.com>
2021-11-04 19:26:06 -0700

    initialized tests

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,6 +1,17 @@
 #include "gtest/gtest.h"
 
-#include "op_test.hpp"
+
+#include "Div.hpp"
+#include "NegativeOPMock.hpp"
+#include "base.hpp"
+
+TEST(TESTNAME,TESTDESCRIPT){
+       Base* dividend = new NegativeOPMock();
+       Base* divisor = new NegativeOPMock();
+       Base* divide = new Div(dividend,divisor);
+       double result = divide->evaluate();
+       EXPECT_EQ(result,1);
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
