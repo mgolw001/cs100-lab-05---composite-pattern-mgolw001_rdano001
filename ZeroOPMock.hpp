9bccf02eba950741bae00d0b5785bdde2b5f4a84
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-04 19:20:40 -0700

    Update ZeroOPMock.hpp

--- ZeroOPMock.hpp
+++ ZeroOPMock.hpp
@@ -5,7 +5,7 @@
 
 class ZeroOPMock : public Base {
     public:
-        ZeroOPMock();
+        ZeroOPMock(): Base() { }
         virtual double evaluate() { return 0.0; }
         virtual std::string stringify() { return "0.0"; }
 };
