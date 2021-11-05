81d3eada883f25dfd3a11ec5a6a652548420d013
RDanover <danover.rachel@gmail.com>
2021-11-04 19:39:01 -0700

    Finalized Unit tests for Div class

--- unit_test.cpp
+++ unit_test.cpp
@@ -3,15 +3,35 @@
 
 #include "Div.hpp"
 #include "NegativeOPMock.hpp"
+#include "SevenOPMock.hpp"
+#include "ZeroOPMock.hpp"
 #include "base.hpp"
 
-TEST(TESTNAME,TESTDESCRIPT){
+TEST(DivideTest,DividingBySameNum){
        Base* dividend = new NegativeOPMock();
        Base* divisor = new NegativeOPMock();
        Base* divide = new Div(dividend,divisor);
        double result = divide->evaluate();
        EXPECT_EQ(result,1);
 }
+
+TEST(DivideTest,DividingByDifNum){
+       Base* dividend = new NegativeOPMock();
+       Base* divisor = new SevenOPMock();
+       Base* divide = new Div(dividend,divisor);
+       double result = divide->evaluate();
+       EXPECT_EQ(result,-0.2);
+}
+
+TEST(DivideTest,DividingWithZero){
+       Base* dividend = new ZeroOPMock();
+       Base* divisor = new SevenOPMock();
+       Base* divide = new Div(dividend,divisor);
+       double result = divide->evaluate();
+       EXPECT_EQ(result,0);
+}
+
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
